6b1b46135ceacba8cd8b199bc7427159528f4d58
Luke Edwards <luke.edwards05@gmail.com>
2017-04-02 23:28:42 -0700

    add favicon.ico server route

--- src/server/index.js
+++ src/server/index.js
@@ -11,10 +11,8 @@ express()
 	.use(compression({ threshold: 0 }))
 	.use('/static', express.static(pubDir, { maxAge }))
 	.use('/sw.js', express.static(`${pubDir}/sw.js`, { setHeaders }))
+	.get('favicon.ico', (_, res) => res.sendFile(`${pubDir}/favicon.ico`))
 	.use(require('./routes'))
-	.get('*', (_, res) => {
-		res.sendFile(`${pubDir}/index.html`);
-	})
 	.listen(PORT, err => {
 		if (err) throw err
 		console.log(`> Ready on http://localhost:${PORT}`)
