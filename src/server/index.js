5e974b581aa9f0cb73d5400b99c392c06756857c
Luke Edwards <luke.edwards05@gmail.com>
2017-04-04 13:43:44 -0700

    use serve-favicon middleware

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,6 @@
 const { join } = require('path');
 const compression = require('compression');
+const favicon = require('serve-favicon');
 const express = require('express');
 
 const maxAge = 31536000; // 1yr
@@ -9,9 +10,9 @@ const pubDir = join(__dirname, 'static');
 express()
 	.disable('x-powered-by')
 	.use(compression({ threshold: 0 }))
+	.use(favicon(`${pubDir}/favicon.ico`))
 	.use('/static', express.static(pubDir, { maxAge }))
 	.use('/sw.js', express.static(`${pubDir}/sw.js`, { setHeaders }))
-	.get('favicon.ico', (_, res) => res.sendFile(`${pubDir}/favicon.ico`))
 	.use(require('./routes'))
 	.listen(PORT, err => {
 		if (err) throw err
