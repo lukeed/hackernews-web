8895d4a6a9a8895aca8da919bd2508549cc78e96
Luke Edwards <luke.edwards05@gmail.com>
2017-04-21 12:45:14 -0700

    clean top-level mounting

--- src/index.js
+++ src/index.js
@@ -1,33 +1,12 @@
 import { h, render } from 'preact';
+import App from './views';
 import './index.sass';
 
-if (process.env.NODE_ENV === 'production') {
-	const App = require('./views').default;
-	render(<App />, document.body);
+render(<App />, document.body);
 
+if (process.env.NODE_ENV === 'production') {
 	// cache all assets if browser supports serviceworker
 	if ('serviceWorker' in navigator && location.protocol === 'https:') {
 		navigator.serviceWorker.register('/sw.js');
-	}
-
-	// add Google Analytics
-	// (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	// (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	// m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	// })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-	// ga('create', 'UA-XXXXXXXX-X', 'auto');
-	// ga('send', 'pageview');
-} else {
-	let elem, App;
-	function init() {
-		App = require('./views').default;
-		elem = render(<App />, document.body, elem);
-	}
-
-	init();
-
-	// listen for HMR
-	if (module.hot) {
-		module.hot.accept('./views', init);
 	}
 }
